268516ba5cb3130f3564f51bd516e4ae0f5f488e
Luca Burelli <l.burelli@arduino.cc>
2026-01-09 16:09:05 +0100

    ci: refactor 2/7: generate per-board test list and batch compile
    Instead of compiling a fixed Blink sketch for each board, generate a
    list of all examples and libraries to compile based on the variant (and
    core). This allows to skip examples that are not applicable for a
    given variant (e.g. using hardware features not present on the board),
    and to add additional libraries and examples to the test suite as
    required.
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

---
+++ extra/artifacts/zephyr_unoq.test_setup.sh
@@ -0,0 +1,16 @@
+# Copyright ARDUINO SRL (https://www.arduino.cc)
+# SPDX-License-Identifier: Apache-2.0
+
+# This script is sourced from extra/ci_test_list.sh to provide
+# artifact-specific tests for Zephyr CI tests.
+#
+# Two helper functions are provided for easy GitHub queries:
+# - get_branch_tip <repo> <branch> [<path> ...]
+# - get_latest_release <repo> [<path> ...]
+#
+# By default, the whole project will be added to the test suite.
+# When given additional path arguments, the functions will only
+# copy artifacts under the provided paths.
+
+# ArduinoBLE
+get_branch_tip libraries arduino-libraries/ArduinoBLE master
