d671d0f666ff19c7280b9bff02830e86de770087
Luca Burelli <l.burelli@arduino.cc>
2026-02-12 18:39:59 +0100

    cores/arduino/api: download the Arduino API as a blob
    Use the 'west blobs' machinery for Zephyr modules to provide the
    interface for downloading the Zephyr API. This allows us to remove the
    submodule and the install script, and to avoid having to rely on a
    symlink or manual steps in the installation instructions.
    
    During the build process, the source archive is extracted in a
    version-specific directory under zephyr/blobs/ArduinoCore-API, and then
    copied into the cores/arduino/api folder. This is done only once per
    API version.
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

--- extra/bootstrap.sh
+++ extra/bootstrap.sh
@@ -50,6 +50,7 @@ log_msg "group" "Installing Zephyr SDK 0.16.8: $NEEDED_TOOLCHAINS"
 west sdk install --version 0.16.8 -t $NEEDED_TOOLCHAINS
 log_msg "endgroup"
 
+NEEDED_HALS="arduino-api $NEEDED_HALS"
 log_msg "group" "Fetching blobs for: $NEEDED_HALS"
 west blobs fetch $NEEDED_HALS
 log_msg "endgroup"
