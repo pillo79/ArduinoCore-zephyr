5de0323dc25622ea97fba6aba506e41f2e4781d3
pennam <m.pennasilico@arduino.cc>
2026-03-30 15:10:18 +0200

    llext: add strdup to exports

--- cores/arduino/llext_wrappers.c
+++ cores/arduino/llext_wrappers.c
@@ -80,6 +80,7 @@ W3(int, memcmp, const void *, const void *, unsigned int)
 W3(void *, memset, void *, int, unsigned int)
 W2(char *, strtok, char *, const char *)
 W3(void *, memchr, const void *, int, size_t)
+W1(char *, strdup, const char *)
 
 /* stdlib.h - conversion */
 W2(double, strtod, const char *, char **)
