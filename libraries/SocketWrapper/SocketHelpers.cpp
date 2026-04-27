78a49f3fc494b795476648542ff3be4fb27a09ae
Andrea Gilardoni <a.gilardoni@arduino.cc>
2026-04-27 08:32:45 +0200

    net_iface: removed event handler
    removing event handler, since they provide only logging information that
    is always discarded

--- libraries/SocketWrapper/SocketHelpers.cpp
+++ libraries/SocketWrapper/SocketHelpers.cpp
@@ -9,60 +9,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(sketch, LOG_LEVEL_NONE);
 
-struct net_mgmt_event_callback NetworkInterface::mgmt_cb;
-struct net_dhcpv4_option_callback NetworkInterface::dhcp_cb;
-
-void NetworkInterface::event_handler(struct net_mgmt_event_callback *cb, uint64_t mgmt_event,
-									 struct net_if *iface) {
-	int i = 0;
-
-	ARG_UNUSED(cb);
-
-	if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
-		return;
-	}
-
-	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
-		char buf[NET_IPV4_ADDR_LEN];
-
-		if (iface->config.ip.ipv4->unicast[i].ipv4.addr_type != NET_ADDR_DHCP) {
-			continue;
-		}
-
-		LOG_INF("   Address[%d]: %s", net_if_get_by_iface(iface),
-				net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[i].ipv4.address.in_addr, buf,
-							  sizeof(buf)));
-		LOG_INF(
-			"    Subnet[%d]: %s", net_if_get_by_iface(iface),
-			net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[i].netmask, buf, sizeof(buf)));
-		LOG_INF("    Router[%d]: %s", net_if_get_by_iface(iface),
-				net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
-		LOG_INF("Lease time[%d]: %u seconds", net_if_get_by_iface(iface),
-				iface->config.dhcpv4.lease_time);
-	}
-}
-
-void NetworkInterface::option_handler(struct net_dhcpv4_option_callback *cb, size_t length,
-									  enum net_dhcpv4_msg_type msg_type, struct net_if *iface) {
-	char buf[NET_IPV4_ADDR_LEN];
-
-	ARG_UNUSED(length);
-	ARG_UNUSED(msg_type);
-	ARG_UNUSED(iface);
-
-	LOG_INF("DHCP Option %d: %s", cb->option, net_addr_ntop(AF_INET, cb->data, buf, sizeof(buf)));
-}
-
 int NetworkInterface::dhcp() {
-	net_mgmt_init_event_callback(&mgmt_cb, event_handler,
-								 NET_EVENT_IPV4_ADDR_ADD | NET_EVENT_IF_UP | NET_EVENT_IF_DOWN);
-	net_mgmt_add_event_callback(&mgmt_cb);
-
-	net_dhcpv4_init_option_callback(&dhcp_cb, option_handler, DHCP_OPTION_NTP, ntp_server,
-									sizeof(ntp_server));
-
-	net_dhcpv4_add_option_callback(&dhcp_cb);
-
 	net_dhcpv4_start(netif);
 
 	LOG_INF("DHCPv4 started...\n");
